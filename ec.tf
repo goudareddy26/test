8574c840412e560ce85d19ef764faa4361b9c897
goudareddy26 <32597856+goudareddy26@users.noreply.github.com>
2023-12-10 16:43:32 +0530

    Update ec.tf

--- ec.tf
+++ ec.tf
@@ -1,12 +1,12 @@
 
 provider "aws" {
- region = "us-east-2"
+ region = "ap-south-1"
  
  }
 
 
 resource "aws_instance" "terraform-demo-host" {
-  ami = "ami-09f85f3aaae282910"
+  ami = "ami-02a2af70a66af6dfb"
   key_name = "gsgogi03"
   instance_type = "t2.micro"
   
